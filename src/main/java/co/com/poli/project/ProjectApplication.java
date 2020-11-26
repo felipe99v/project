17c390e93d9ad7bacef679a0de75561c688a41fd
SantiagoMedinaNRA <santiago.medina@numrot.com>
2020-11-26 18:44:58 -0500

    Se devuelve cambio

--- src/main/java/co/com/poli/project/ProjectApplication.java
+++ src/main/java/co/com/poli/project/ProjectApplication.java
@@ -2,10 +2,8 @@ package co.com.poli.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class ProjectApplication {
 
     public static void main(String[] args) {
